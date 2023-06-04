f4e445e600f1b1773f0e6a5199ba4c4a32e71535
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-06-04 20:12:51 +0700

    change app name

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Flight API'),
+    'name' => env('APP_NAME', 'Aero Reserve'),
 
     /*
     |--------------------------------------------------------------------------
